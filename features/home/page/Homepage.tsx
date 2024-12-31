0f12e544e4c3771ab009f75d3740e3592ecb2cab
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-12-30 23:17:57 -0300

    feat: add animation in Homepage

--- features/home/page/Homepage.tsx
+++ features/home/page/Homepage.tsx
@@ -1,17 +1,23 @@
+"use client"
 import styles from './styles.module.css'
-import Image from 'next/image'
-
+import React from 'react';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import {} from '../../../public/invest-animation.json'
 export const HomePage = () => {
   return (
     <>
       <article className={ styles['content-text']}>
       <h2> 
-        <span>PureInvest</span> 
-        Transformando dicas em oportunidades para você investir com confiança e alcançar seus objetivos financeiros.
+        <span>PureInvest </span> 
+         Transformando dicas em oportunidades para você investir com confiança e alcançar seus objetivos financeiros.
       </h2>
     </article>
     <article className={ styles['content-img']}>
-      <img src="/transparent-bull.png" alt="" />
+    <DotLottieReact
+      src='https://lottie.host/3bbc4855-b15f-4dda-be58-abe5b553a335/woS4GNx7Gv.lottie'
+      loop
+      autoplay
+    />
     </article>
     </>
   )
